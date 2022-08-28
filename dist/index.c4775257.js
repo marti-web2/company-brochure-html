4183d1ff6eff194adaed1242cc8c486d5980286f
David <anddagger@gmail.com>
2022-08-28 01:40:46 +0000

    added navbar collapse

--- dist/index.c4775257.js
+++ dist/index.c4775257.js
@@ -1,2 +1,10 @@
+// excute script after page load
+window.onload = function() {
+    const toggle = document.querySelector("#nav .toggle-btn");
+    const collapse = document.querySelector("#nav .collapse");
+    toggle.addEventListener("pointerdown", function(e) {
+        collapse.classList.toggle("active");
+    });
+};
 
 //# sourceMappingURL=index.c4775257.js.map
---
+++ main.js
@@ -0,0 +1,10 @@
+// excute script after page load
+window.onload = function() {
+  const toggle=document.querySelector("#nav .toggle-btn")
+  const collapse=document.querySelector("#nav .collapse")
+
+  toggle.addEventListener("pointerdown",function(e){
+    collapse.classList.toggle('active')
+  }
+  )
+}
